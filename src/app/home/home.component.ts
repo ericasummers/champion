86e7fff829d98e7d981dd3867e7c646f8c1f62c1
Erica Wright and jay freeman <jay@>
2017-04-05 10:45:55 -0700

    display project titles from firebase on homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { ProjectService } from '../project.service';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ProjectService]
 })
 export class HomeComponent implements OnInit {
+  projects: FirebaseListObservable<any[]>;
 
-  constructor() { }
+
+  constructor(private projectService: ProjectService) { }
 
   ngOnInit() {
+    this.projects = this.projectService.getProjects();
   }
 
 }
---
+++ src/app/project.model.ts
@@ -0,0 +1,3 @@
+export class Project {
+  constructor(public url: string, public categories: string, public creator: string, public creatorImage: string, public date: string, public deadline: string, public description: string, public goal: number, public location: string, public title: string) { }
+}
